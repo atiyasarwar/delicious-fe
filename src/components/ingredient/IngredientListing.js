033f65909ee461208419c432055fdf44b653c844
Atiya Sarwar <faiq.mahmood@koderlabs.com>
2022-07-17 23:13:32 +0500

    bug fix

--- src/components/ingredient/IngredientListing.js
+++ src/components/ingredient/IngredientListing.js
@@ -154,7 +154,9 @@ export default function IngredientListing() {
                     <button
                       className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
                       onClick={() => setCurrentPageCount(currentPageCount + 20)}
-                      disabled={!!(currentPageCount >= ingredientList.count)}
+                      disabled={
+                        !!(currentPageCount + 20 >= ingredientList.count)
+                      }
                     >
                       Next
                     </button>
